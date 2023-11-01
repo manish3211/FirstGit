4d3ceff025ba32bcea3f56f8fedf09e9538c1888
manish3211 <manishvalechha290898@gmail.com>
2023-11-02 02:28:09 +0530

    ClassName done

--- dom.js
+++ dom.js
@@ -27,5 +27,15 @@ var header=document.getElementById('main-header');
 
 header.style.borderBottom='solid 3px #000';
 
-document.getElementById('main').style.fontWeight='bold';
-document.getElementById('main').style.color='green';
+// document.getElementById('main').style.fontWeight='bold';
+// document.getElementById('main').style.color='green';
+
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+
+// GIVES ERROR
+// items.style.fontWeight='bold';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
