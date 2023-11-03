85a1a75c8fea652e3451154299b621a5d2a2ab9f
manish3211 <manishvalechha290898@gmail.com>
2023-11-03 15:04:07 +0530

    Filter done

--- deledit.js
+++ deledit.js
@@ -1,26 +1,43 @@
 var form=document.getElementById('addForm');
-var itemList=document.getElementById('items');
+var item=document.getElementById('items');
+var itemList=document.getElementsByClassName('list-group-item');
 var filter=document.getElementById('filter');
 
 
 form.addEventListener('submit',addItem);
 
-itemList.addEventListener('click',removeItem);
+item.addEventListener('click',removeItem);
 
 filter.addEventListener('keyup',filterItems);
+
+
+
+for(let i=0;i<itemList.length;i++){
+    var editBtn=document.createElement('button');
+
+    editBtn.className='btn btn-sm float-right editBtn';
+
+    editBtn.appendChild(document.createTextNode('Edit'));
+
+    itemList[i].appendChild(editBtn);
+}
+
 
 
 function addItem(e){
     e.preventDefault();
 
     var newItem=document.getElementById('item').value;
+    var description=document.getElementById('description').value;
 
 
     var li=document.getElementById('li');
 
     li.className='list-group-item';
 
+
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(description));
 
 
     var deleteBtn=document.getElementById('button');
@@ -31,6 +48,7 @@ function addItem(e){
 
     li.appendChild(deleteBtn);
 }
+
 
 
 function removeItem(e){
@@ -46,17 +64,18 @@ function removeItem(e){
 function filterItems(e){
     var text=e.target.value.toLowerCase();
 
-    var items=itemList.getElementsByTagName("li");
+    var items=itemList.getElementsByTagName('li');
 
     Array.from(items).forEach(function(item){
         var itemName=item.firstChild.textContent;
+        var description=item.childNodes[1].textContent;
 
-        if(item.toLowerCase().indexOf(text) != -1){
-            item.style.display="block";
+        if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+            item.style.display='block';
         }
 
         else{
-            item.style.display="none";
+            item.style.display='none';
         }
     });
 }
